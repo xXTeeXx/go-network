e82d3f7f59ad1d900c10e1040e9763189837e23d
xXTeeXx <woramett2015@gmail.com>
2023-11-20 21:21:08 +0700

    Add new hello funcion

--- tutor1/hello.go
+++ tutor1/hello.go
@@ -41,4 +41,79 @@ func main(){
 
 	var s []float64 = arry5[0:2]
 	fmt.Println(s)
+
+	type person struct {
+		name string
+		age int
+	}
+	var p person
+	p.name ="woramet"
+	p.age=40
+	fmt.Println(p)
+
+	var x int =5 
+	var xptr *int= &x
+	fmt.Println(xptr)
+
+	for i:=0; i < 5; i++ {
+		fmt.Println(i)
+	  }
+
+	  var a = make(map[string]string)
+	  var b map[string]string
+	
+	  fmt.Println(a == nil)
+	  fmt.Println(b == nil)
+
+	  month := 5
+
+    switch month {
+    case 1:
+        fmt.Println("January")
+    case 2:
+        fmt.Println("February")
+    case 3:
+        fmt.Println("March")
+    case 4:
+        fmt.Println("April")
+    case 5:
+        fmt.Println("May")
+    case 6:
+        fmt.Println("June")
+    case 7:
+        fmt.Println("July")
+    case 8:
+        fmt.Println("August")
+    case 9:
+        fmt.Println("September")
+    case 10:
+        fmt.Println("October")
+    case 11:
+        fmt.Println("November")
+    case 12:
+        fmt.Println("December")
+    default:
+        fmt.Println("Invalid month")
+    }
+
+	q:= 20
+  i:= 18
+  if q > i {
+    fmt.Println("x is greater than y")
+  }
+
+  time := 5
+  if time < 10 {
+    fmt.Println("Good morning.")
+  } else if time < 20 {
+    fmt.Println("Good day.")
+  } else {
+    fmt.Println("Good evening.")
+  }
+  
+  var k = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+  l := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+
+  fmt.Printf("k\t%v\n", k)
+  fmt.Printf("l\t%v\n", l)
 }
