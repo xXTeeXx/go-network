6d58d96e6886ce082db3aee63a22e086c6809ef4
xXTeeXx <woramett2015@gmail.com>
2024-01-09 13:51:24 +0700

    uodate

--- go-web/go.mod
+++ go-web/go.mod
@@ -1,3 +1,5 @@
 module github.com/xXTeeXx/go-network
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
--- go-web/main.go
+++ go-web/main.go
@@ -3,15 +3,29 @@ package main
 import (
     "fmt"
     "net/http"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
+	route := mux.NewRouter()
+
+    route.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
     })
 
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    http.ListenAndServe(":8080", route)
 
-    http.ListenAndServe(":80", nil)
+
+    // http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    //     fmt.Fprintf(w, "Welcome to my website!")
+    // })
+
+    // fs := http.FileServer(http.Dir("static/"))
+    // http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    // http.ListenAndServe(":80", nil)
 }
